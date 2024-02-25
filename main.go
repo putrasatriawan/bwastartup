75982825ac09d5004f14a69ce572003cecc3176c
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2024-02-25 15:23:02 +0700

    users to json

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"bwastartup/user"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -22,15 +24,30 @@ func main() {
 	fmt.Println("Connection to database is good")
 
 	var users []user.User
-
-	length := len(users)
-	fmt.Println(length)
-
 	db.Find(&users)
-	fmt.Println(length)
 
 	for _, user := range users {
 		fmt.Println(user.Name)
 		fmt.Println(user.Email)
 	}
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context){
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	//jika ada error maka cek error nya apa
+	if err != nil{
+		log.Fatal((err.Error()))
+	}
+
+	var users []user.User
+	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
+
 }
